64166a56f158c15ff57dfcd70413c15765e0e16d
remruatpuii1 <76507276+remruatpuii1@users.noreply.github.com>
2021-09-09 21:33:13 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -14,11 +14,10 @@ var fx,fx1,fx2,fx3;
 var peach,ph1,peachGroup;
 var score = 0;
 var vis = 255;
-var count = 0;
 var gameState = "play";
-var num=0;
 var hearts=[];
-
+var edges;
+var processingState=false;
 function preload()
 {
   // Loading up the images & animations 
@@ -66,16 +65,16 @@ function preload()
 function setup() 
 {
   createCanvas(windowWidth,windowHeight);
-
+  edges=createEdgeSprites();
   engine = Engine.create();
   world = engine.world;
 
-  rh = createSprite(450,550,30,30);
+  rh = createSprite(width/2,height-50,30,30);
   rh.addImage("idle",rhi);
   rh.scale=0.450;
   rh.setCollider("rectangle",0,0,250,400);
 
-  floor = createSprite(1,570,2500,10);
+  floor = createSprite(width/2,height-50,2500,10);
   floor.visible=false;
 
   for(i=0;i<=2;i++){
@@ -96,12 +95,14 @@ function draw()
 {
   background(bg1,windowWidth,windowHeight); 
   textSize(30);
+  fill("white");
   text("Score: "+score,width-150,50);
   Engine.update(engine);
   rh.collide(floor);
- 
+  rh.collide(edges);
   if(gameState === "play")
   {
+
     rh.velocityY += 0.8;
     
   if(keyDown("right_arrow"))
@@ -132,39 +133,52 @@ function draw()
       fx1.play();
   }
 
+  if(elGroup.isTouching(rh))
+  {
+    score +=10;
+    if(hearts.length<=6){
+      console.log(hearts.length,"eating hearts");
+        var l3=hearts.length+1;
+        var fhrt=createSprite(70*l3,50,10,10);
+        fhrt.addImage("life",l1);
+        fhrt.scale=0.1;
+        hearts.push(fhrt);
+    }
+      elGroup.destroyEach();
+    }
   if(spikeGroup.isTouching(rh) || zmGroup.isTouching(rh) || zfGroup.isTouching(rh))
   {
     fx3.play();
-    gameState="Life";
+    if(hearts.length>1)
+    {
+      console.log(hearts.length,"minus 1 heart");
+      var l=hearts.length;
+      rh.x=width/2;
+      rh.y=-50;
+      rh.velocityY=2;
+      hearts[l-1].destroy();
+      hearts.pop();
+      push();
+      vis -=1;
+      tint(255,vis);
+      image(l1,(l*70)-20,27,45,42);
+      pop();  
+    }
+    else if (hearts.length<=1)
+    {
+      console.log(hearts.length,"empty array");
+      var l=hearts.length;
+      hearts[l-1].destroy();
+      hearts.pop();
+      gameState="End";
+     
+    }
   }
   if(peachGroup.isTouching(rh))
   {
     score +=20;
     peachGroup.destroyEach();
   }
-
-  if(elGroup.isTouching(rh))
-  {
-    score +=10;
-    if(num===0)
-    {
-        var l3=hearts.length+1;
-        console.log(l3);
-        var fhrt=createSprite(70*l3,50,10,10);
-        fhrt.addImage("life",l1);
-        fhrt.scale=0.1;
-        hearts.push(fhrt);
-        num=1;
-      }
-    else if(num===1){
-      var l2=hearts.length+1;
-      var thrt=createSprite(70*l2,50,10,10);
-      thrt.addImage("life",l1);
-      thrt.scale=0.1;
-      hearts.push(thrt);
-      }
-      elGroup.destroyEach();
-    }
   
   createSpikes();
   randomHearts();
@@ -172,131 +186,12 @@ function draw()
   zombieGirl();
   createPeach();
   }
-
-else if(gameState==="Life")
-{
-  rh.velocityY += 0.8;
-    
-  if(keyDown("right_arrow"))
-  {
-    rh.addAnimation("idle",rhr);
-    rh.velocityX =8;
-    rh.x +=5;
-  }
-
-  if(keyDown("left_arrow") && rh.x>10)
-  {
-    rh.addAnimation("idle",lrhr);
-    rh.velocityX =-8;
-    rh.x -=5;
-  }
-
-  if(keyDown("space") && rh.y>450 && rh.velocityX +8 )
-  {
-    rh.addAnimation("idle",rhj);
-      rh.velocityY = -18;
-      fx1.play();
-  }
-  
-  if(keyDown("space") && rh.y>450 && rh.velocityX -8 )
-  {
-    rh.addAnimation("idle",lrhj);
-      rh.velocityY = -18;
-      fx1.play();
-  }
-
-  if(peachGroup.isTouching(rh))
-  {
-    score +=10;
-    peachGroup.destroyEach();
-  }
-
-  if(elGroup.isTouching(rh))
-  {
-    score +=10;
-    if(num===0)
-    {
-        var len=hearts.length+1;
-        var nhrt=createSprite(70*len,50,10,10);
-        nhrt.addImage("life",l1);
-        nhrt.scale=0.1;
-        hearts.push(nhrt);
-        num=1;
-      }
-    else if(num===1){
-      var len=hearts.length+1;
-      var shrt=createSprite(70*len,50,10,10);
-        nhrt.addImage("life",l1);
-        nhrt.scale=0.1;
-        hearts.push(shrt);
-      }
-      elGroup.destroyEach();
-    }
-  
-
-  if(rh.isTouching(zmGroup) || rh.isTouching(zfGroup) || rh.isTouching(spikeGroup))
-  {
-  count+=1;
-  console.log(count);
-  if(count===1)
-  {
-    var l=hearts.length;
-    hearts[l-1].destroy();
-    push();
-    vis -=5;
-    tint(255,vis);
-    image(l1,190,30,45,45);
-    pop();
-  }
-}
-    if(rh.isTouching(zmGroup) || rh.isTouching(zfGroup) || rh.isTouching(spikeGroup))
-  {
-    count+=1;
-    console.log(count);
-   if(count===2)
-  {
-    var l=hearts.length;
-    hearts[l-1].destroy();
-    push();
-    vis -=5;
-    tint(255,vis);
-    image(l1,150,30,45,45);
-    pop();
-
-  }
-  }
-
-  if(rh.isTouching(zmGroup) || rh.isTouching(zfGroup) || rh.isTouching(spikeGroup))
-{
-  count+=1;
-  console.log(count);
- if(count===3)
-  {
-    var l=hearts.length;
-    hearts[l-1].destroy();
-    push();
-    vis -=5;
-    tint(255,vis);
-    image(l1,100,30,45,45);
-    pop();
-    fx3.play();
-    gameState="End";
-  }
-}
-  
-  createSpikes();
-  randomHearts();
-  zombieBoy();
-  zombieGirl();
-  createPeach();
-  
-}
-
 else if (gameState==="End")
 {
   rh.addImage("idle",rhd);
   rh.velocityX=0;
   rh.velocityY=0;
+  rh.y=height-140;
   zmGroup.destroyEach();
   zfGroup.destroyEach();
   spikeGroup.destroyEach();
@@ -305,7 +200,10 @@ else if (gameState==="End")
   zmGroup.setVelocityXEach = 0;
   zfGroup.setVelocityXEach = 0;
   textSize(70);
-  text("Game Over",400,350);
+  fill("white");
+  text("Game Over",width/2.5,height/2.5);
+  textSize(30);
+  text("Press R key to play again!", width/2.5,height/2);
 }
 
   drawSprites();
@@ -316,7 +214,7 @@ function createSpikes()
 {
   if(frameCount%200===0)
   {
-  spike = createSprite(random(10,width-10),540,20,20);
+  spike = createSprite(random(10,width-10),height-100,20,20);
   spike.addAnimation("spike",s1);
   spike.scale=0.50;
   spikeGroup.add(spike);
@@ -329,30 +227,31 @@ function createSpikes()
 
 function randomHearts()
 {
-  if(frameCount%500===0)//1900
+  var check=hearts.length;
+  if(check<=6 && frameCount%1900===0)
   {
     el = createSprite(random(10,width-10),-50,20,20);
     el.addImage("extra",el1);
     el.velocityY=10;
     el.scale=0.130;
     elGroup.add(el);
-    el.lifetime=height/10
+    el.lifetime=height/10;
 
   }
 }
 
 function zombieBoy()
 {
-  if(frameCount%400===0)//800
+  if(frameCount%800===0)
   {
-    zm = createSprite(-20,480,10,10);
+    zm = createSprite(-20,height-150,10,10);
     zm.addAnimation("zombieBoy",zmw);
     zm.scale=0.40;
-    zm.velocityX=(3+frameCount/100);
+    zm.velocityX=(2+frameCount/100);
     zmGroup.add(zm);
     zm.lifetime=width/zm.velocityX;
     zm.setCollider("rectangle",0,0,200,350);
-    if(frameCount%800===0)//1600
+    if(frameCount%1600===0)
     {
       zmGroup.destroyEach();
     }
@@ -361,16 +260,16 @@ function zombieBoy()
 
 function zombieGirl()
 {
-  if(frameCount%800===0)//1500
+  if(frameCount%1500===0)
   {
-    zf = createSprite(width*2,480,10,10);
+    zf = createSprite(width*2,height-150,10,10);
     zf.addAnimation("zombieGirl",zfw);
     zf.scale=0.40;
-    zf.velocityX=(-5+frameCount/100); 
+    zf.velocityX=(-4+frameCount/100); 
     zfGroup.add(zf);
     zf.lifetime=width/zf.velocityX;
     zf.setCollider("rectangle",0,0,200,350);
-    if(frameCount%1600===0)//3000
+    if(frameCount%3000===0)
     {
       zfGroup.destroyEach();
     }
@@ -380,18 +279,35 @@ function zombieGirl()
 
 function createPeach()
 {
-  if(frameCount%500===0)//800
+  if(frameCount%800===0)
   {
   peach = createSprite(random(10,width-10),60,10,10);
   peach.addImage("peach",ph1);
   peach.scale=0.080;
   peach.velocityY=6;
   peachGroup.add(peach);
-  peach.lifetime=height/peach.velocityY
-  if(frameCount%1000===0)//1600
+  peach.lifetime=height/peach.velocityY;
+  if(frameCount%1600===0)
   {
     peachGroup.destroyEach();
   }
   }
 
+}
+function keyPressed()
+{
+  if(keyCode===114 || keyCode===82){
+    score=0;
+    for(j=0;j<=2;j++){
+      var l4=hearts.length+1;
+      var khrt=createSprite(70*l4,50,10,10);
+      khrt.addImage("life",l1);
+      khrt.scale=0.1;
+      hearts.push(khrt);
+      }
+      rh.addImage("idle",rhi);
+      rh.scale=0.450;
+    gameState="play";
+
+  }
 }
